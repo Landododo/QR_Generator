c7a2aef7eedf7841d3ec24568c580324f200a603
Perry Han <perryhan0123@gmail.com>
2025-01-17 14:34:02 -0800

    UI tweaks

--- QR_gen_new.py
+++ QR_gen_new.py
@@ -41,6 +41,7 @@ def main():
     lib = gdspy.GdsLibrary()
     lib.unit = 1.0e-6
     # generator = GDSIIQRGenerator(50,lib)
+    use_interactive = False
     global hum_text, reduction, precision
     name, length, height, qr_size, spacing, abs_pos, no_size, padding, forced_version, error_correction, num_modules_long,precision, reduction, hum_text = (None,None,None,None,None,None,None,None,None,None,None, None,None,None)
     if len(sys.argv) > 1:
@@ -49,6 +50,7 @@ def main():
         qrs_in_col = int(round((height + spacing) / (qr_size + spacing), 5))
         length, height, num_modules_long, spacing = adjust_qr_size_and_padding(length, height, padding, False, None, ec_level, abs_pos, qrs_in_row, qrs_in_col, spacing, False)
     else:
+        use_interactive = True
         name, length, height, qr_size, spacing, abs_pos, no_size, no_spacing = input_data()
         padding, hum_text, reduction, precision, forced_version, error_correction = default_overides(qr_size)
         qrs_in_row = int(round((length + spacing) / (qr_size + spacing), 5))
@@ -93,7 +95,7 @@ def main():
         else:
             fname = fnameBase
         if not os.path.exists(os.path.join(path,fname+'.gds')):
-            fname = fname + '.gds'
+            fname+='.gds'
             break
         i += 1
 
@@ -102,6 +104,8 @@ def main():
     sys.stdout.flush()
     lib.write_gds(fname)
     print('Done.')
+    if use_interactive:
+        msgbox(f"Done generating! Took {time.time() - start:.2f} seconds. File saved at {fname}.", "QR Generator")
 
 if __name__ == '__main__':
     main()
